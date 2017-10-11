ed7c10d05d94ae1cec6cb739cb1fbd000ebab406
Michael Reda <michaelreda96@gmail.com>
2017-10-11 17:35:13 +0200

    adding chatbot server to same directory

--- main.go
+++ main.go
@@ -4,47 +4,48 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/ramin0/chatbot"
+	"github.com/mekladious/botify/chatbotserver"
 )
 
 // Autoload environment variables in .env
 import _ "github.com/joho/godotenv/autoload"
 
-// func chatbotProcess(session chatbot.Session, message string) (string, error) {
-// 	if strings.EqualFold(message, "chatbot") {
-// 		return "", fmt.Errorf("This can't be, I'm the one and only %s!", message)
-// 	}
+func chatbotProcess(session chatbot.Session, message string) (string, error) {
+	if strings.EqualFold(message, "chatbot") {
+		return "", fmt.Errorf("This can't be, I'm the one and only %s!", message)
+	}
 
-// 	var questionMarksCount int
-// 	// Try fetching the count of question marks
-// 	count, found := session["questionMarksCount"]
-// 	// If a count is saved in the session
-// 	if found {
-// 		// Cast it into an int (since sessions values are generic)
-// 		questionMarksCount = count.(int)
-// 	} else {
-// 		// Otherwise, initialize the count to 1
-// 		questionMarksCount = 1
-// 	}
+	var questionMarksCount int
+	// Try fetching the count of question marks
+	count, found := session["questionMarksCount"]
+	// If a count is saved in the session
+	if found {
+		// Cast it into an int (since sessions values are generic)
+		questionMarksCount = count.(int)
+	} else {
+		// Otherwise, initialize the count to 1
+		questionMarksCount = 1
+	}
 
-// 	// Build the question marks string according to the question marks count
-// 	var questionMarks string
-// 	for i := 1; i <= questionMarksCount; i++ {
-// 		questionMarks += "?"
-// 	}
+	// Build the question marks string according to the question marks count
+	var questionMarks string
+	for i := 1; i <= questionMarksCount; i++ {
+		questionMarks += "?"
+	}
 
-// 	// Save the updated question marks count to the session
-// 	session["questionMarksCount"] = questionMarksCount + 1
+	// Save the updated question marks count to the session
+	session["questionMarksCount"] = questionMarksCount + 1
 
-// 	// Return the response with an extra question mark
-// 	return fmt.Sprintf("Hello <b>%s</b>, my name is chatbot. What was yours again%s", message, questionMarks), nil
-// }
+	// Return the response with an extra question mark
+	return fmt.Sprintf("Hello <b>%s</b>, my name is chatbot. What was yours again%s", message, questionMarks), nil
+}
 
 func main() {
 	// Uncomment the following lines to customize the chatbot
-	// chatbot.WelcomeMessage = "What's your name?"
-	// chatbot.ProcessFunc(chatbotProcess)
+	chatbot.WelcomeMessage = "What's your name?"
+	chatbot.ProcessFunc(chatbotProcess)
 
 	// Use the PORT environment variable
 	port := os.Getenv("PORT")
